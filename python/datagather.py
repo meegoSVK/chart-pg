6fd856eabf554ae5d6dc948ba569570acaa0351e
Meego Smith <meegoSVK@users.noreply.github.com>
2019-02-22 23:13:27 +0100

    Pridana konektivita na PostgreSQL - config file pre konektivitu

--- python/datagather.py
+++ python/datagather.py
@@ -1,6 +1,47 @@
 # -*- coding: utf-8
 # potrebne kniznice. GLOB + OS na pouzivanie FILOV, RE na pracu s textom
-import glob,os,re
+import glob,os,re,psycopg2
+from configparser import ConfigParser
+
+
+# Okopcene z psycopg2 tutorialov
+def config(filename='../ini/database.ini', section='postgresql'):
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
+
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    return db
+
+# okopcene z psycopg2 tutorialov
+# Treba vymysliet nejaky bulk insert, pretoze stale otvaranie kurzorov a commity po jednom riadku su narocne
+def connection(sensid, temperature):
+    conn = None
+    try:
+        params = config()
+        print('Connecting to Database')
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        print('INSERTING DATA FROM SENSOR ' + sensid)
+        cur.execute('INSERT INTO temperature (sensid, temperature) VALUES(%s,%s)', (sensid, temperature))
+        conn.commit()
+        conn.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+
+
 # loop cez vsetky najdene files ktore poskytuju data z cidiel
 for sens_file in sorted(glob.glob('/sys/devices/w1_bus_master1/28*/w1_slave')):
     print("Nacitavam senzor: ")
@@ -24,3 +65,4 @@ for sens_file in sorted(glob.glob('/sys/devices/w1_bus_master1/28*/w1_slave')):
     print('Trosku iny pokus o vystup')
     print('Sensor ID: ' + sensid)
     print('Temperature : ' + temperature + ' °C')
+    connection(sensid, temperature)
