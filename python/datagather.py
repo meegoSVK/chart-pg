3a0340425a690a55429774ffd6afa6dec3edfdd9
Meego Smith <meegoSVK@users.noreply.github.com>
2019-02-22 17:04:20 +0100

    Testovaci python

---
+++ python/datagather.py
@@ -0,0 +1,17 @@
+import glob,os,re
+for sens_file in sorted(glob.glob('/sys/devices/w1_bus_master1/28*/w1_slave')):
+    print("Nacitavam senzor: ")
+    sensid = re.findall('(28-.*)/', sens_file)
+    file = open(sens_file, "r")
+    file_open = file.read()
+    raw_temperature = re.findall('t=([0-9]{5})', file_open)
+    file.close()
+    print(file_open)
+    print('Trosku iny pokus o vystup')
+    print('Sensor ID: ' )
+    print(sensid)
+    print('Raw Temperature: ')
+    print(raw_temperature)
+    print('Temperature output in C')
+    raw_temperature.insert(2,'.')
+    print(''.join(raw_temperature))
